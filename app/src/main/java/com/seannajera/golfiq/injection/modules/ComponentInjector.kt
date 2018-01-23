d17fcd4db8eeb6fe4ec1c57fc4d70ff25519bbd0
seannajera <sn@flink.to>
2018-01-23 15:35:32 -0800

    changed component injector

--- app/src/main/java/com/seannajera/golfiq/injection/modules/ComponentInjector.kt
+++ app/src/main/java/com/seannajera/golfiq/injection/modules/ComponentInjector.kt
@@ -21,30 +21,12 @@ object ComponentInjector {
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
                 handleActivity(activity)
             }
-
-            override fun onActivityStarted(activity: Activity) {
-
-            }
-
-            override fun onActivityResumed(activity: Activity) {
-
-            }
-
-            override fun onActivityPaused(activity: Activity) {
-
-            }
-
-            override fun onActivityStopped(activity: Activity) {
-
-            }
-
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-
-            }
-
-            override fun onActivityDestroyed(activity: Activity) {
-
-            }
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
         })
     }
 
